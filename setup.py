2d8346342b9d055673a1f6efb9f0336483848485
Pedro Algarvio <pedro@algarvio.me>
2020-09-24 10:44:43 +0100

    Mirror: 2019.5.30

--- setup.py
+++ setup.py
@@ -4,5 +4,5 @@ from setuptools import setup
 setup(
     name='pre_commit_dummy_package',
     version='0.0.0',
-    install_requires=['nox==2019.4.11'],
+    install_requires=['nox==2019.5.30'],
 )
