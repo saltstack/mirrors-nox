347978407c2621dc95614608710affb686a3e7a0
Pedro <pedro@algarvio.me>
2021-05-26 08:35:07 +0000

    Mirror: 2020.12.31

--- setup.py
+++ setup.py
@@ -4,5 +4,5 @@ from setuptools import setup
 setup(
     name='pre_commit_dummy_package',
     version='0.0.0',
-    install_requires=['nox==2020.8.22'],
+    install_requires=['nox==2020.12.31'],
 )
